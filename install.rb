8feffa6a253328d730d8d3a56924c8f724bf4656
Scott Watermasysk <scottwater@gmail.com>
2024-11-12 14:14:25 -0500

    refactor and enable lazyvim

--- install.rb
+++ install.rb
@@ -2,21 +2,26 @@ require 'pathname'
 home_path = Dir.home
 script_dir = File.dirname(File.expand_path(__FILE__))
 
+def link_if_needed(symlink, source)
+  if File.symlink?(symlink)
+    puts "#{symlink} is already a symlink"
+    return
+  elsif File.exist?(symlink)
+    puts "#{symlink} already exists, skipping"
+    return
+  end
+
+  puts "Creating symlink for #{source}"
+  system("ln -s #{source} #{symlink}")
+end
+
 dots = %w[gitconfig gitignore_global zshrc zprofile gemrc]
 puts "Installing dotfiles: #{dots.join(', ')}"
 
 dots.each do |dot|
   symlink = File.join(home_path, ".#{dot}")
-  if File.symlink?(symlink)
-    puts "#{symlink} is already a symlink"
-    next
-  elsif File.exist?(symlink)
-    puts "#{symlink} already exists, skipping"
-  else
-    puts "Creating symlink for #{dot}"
-    dot_path = File.join(script_dir, dot)
-    system("ln -s #{dot_path} #{symlink}")
-  end
+  source = File.join(script_dir, dot)
+  link_if_needed(symlink, source)
 end
 
 puts "\n\n"
@@ -25,34 +30,18 @@ config_files = %w[Brewfile zsh]
 puts "Installing config_files: #{config_files.join(', ')}"
 config_files.each do |config|
   symlink = File.join(home_path, config)
-  if File.symlink?(symlink)
-    puts "#{symlink} is already a symlink"
-    next
-  elsif File.exist?(symlink)
-    puts "#{symlink} already exists, skipping"
-  else
-    puts "Creating symlink for #{config}"
-    config_path = File.join(script_dir, config)
-    system("ln -s #{config_path} #{symlink}")
-  end
+  source = File.join(script_dir, config)
+  raise "Source file #{source} does not exist" unless File.exist?(source)
+  link_if_needed(symlink, source)
 end
 
 puts "\n\n"
 
-configs = %w[atuin kitty mise]
+configs = %w[atuin kitty mise nvim]
 puts "Installing configs: #{configs.join(', ')}"
 config_path = File.join(home_path, ".config")
 configs.each do |config|
   symlink = File.join(config_path, config)
-  if File.symlink?(symlink)
-    puts "#{symlink} is already a symlink"
-    next
-  elsif File.exist?(symlink)
-    puts "#{symlink} already exists, skipping"
-  else
-    puts "Creating symlink for #{config}"
-    source_path = File.join(script_dir, "config", config)
-    # system("ln -s #{source_path} #{symlink}")
-    File.symlink(source_path, symlink)
-  end
+  source_path = File.join(script_dir, "config", config)
+  link_if_needed(symlink, source_path)
 end
