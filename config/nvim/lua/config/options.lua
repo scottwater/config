f7d9cb7f8f1a9d42118c6d9f27946f632c497014
Scott Watermasysk <scottwater@gmail.com>
2025-12-16 10:28:04 -0500

    Get started with LazyVim again

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,3 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable spell checking (LazyVim enables it for markdown/text by default)
+-- vim.opt.spell = false
